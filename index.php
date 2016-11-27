c29226849f4d7e3dbf5580de7658fdfd23bcfc2b
roencosa <roencosa@localhost.localdomain>
2016-11-27 05:03:40 +0100

    full paises

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-# mundo
-Lista de paises con sus provincias, y ciudades en diferentes formatos
-
-##Quieres contribuir con la lista? 
-
-Crea la carpeta del país
-Dentro de el pon el fichero con el formato que desas contribuir
-
 Argentina
 Bolivia
 Brasil
